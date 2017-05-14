88cefa20d972d25b213e196a1ac4db85ea10bda9
Radu-Stefan Neacsu <radu-stefan.neacsu@my.fmi.unibuc.ro>
2017-05-14 21:42:47 +0300

    Created client project and added JSON jar

---
+++ Client/src/client/Client.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package client;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+import org.json.JSONObject;
+
+/**
+ *
+ * @author Radu-Stefan Neacsu
+ */
+public class Client extends Application 
+{
+    
+    @Override
+    public void start(Stage primaryStage) 
+    {
+        
+    }
+
+   
+    public static void main(String[] args) 
+    {
+        launch(args);
+    }
+    
+}
