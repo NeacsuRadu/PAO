eb09e134b2cdd91708d8c902a175474c146dfe18
Alexandru Zugravu <Alexandru Zugravu>
2017-05-16 23:59:05 +0300

    16.05.2017

--- Client/src/client/Client.java
+++ Client/src/client/Client.java
@@ -5,27 +5,41 @@
  */
 package client;
 
+import com.sun.javaws.Main;
+import java.io.IOException;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 import org.json.JSONObject;
 
-/**
- *
- * @author Radu-Stefan Neacsu
- */
-public class Client extends Application 
-{
+
+public class Client extends Application {
+    
+    private Stage primaryStage;
+    private BorderPane mainLayout;
     
     @Override
-    public void start(Stage primaryStage) 
-    {
+    public void start(Stage primaryStage) throws IOException {
+        this.primaryStage = primaryStage;
+        this.primaryStage.setTitle("Client App");
+        showMainView();
         
+    }
+    
+    public void showMainView() throws IOException {
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(Main.class.getResource("view/MainView.fxml"));
+        mainLayout = loader.load();
+        Scene scene = new Scene(mainLayout);
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 
    
