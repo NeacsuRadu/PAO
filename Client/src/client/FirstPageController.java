1ebdf0278d34f2a1844e01affb056457ab7f057e
Alexandru Zugravu <Alexandru Zugravu>
2017-05-20 19:31:00 +0300

    passwordField added to First Page

--- Client/src/client/FirstPageController.java
+++ Client/src/client/FirstPageController.java
@@ -16,6 +16,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
@@ -29,7 +30,7 @@ public class FirstPageController implements Initializable {
    @FXML
     private TextField username;
     @FXML
-    private TextField password;
+    private PasswordField password;
     @FXML
     private Label warning;   
     @FXML
