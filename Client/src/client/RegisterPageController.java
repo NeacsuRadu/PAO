c404c42806cc41f5381de7330a2124a034ea0a4c
denisa <em_denisa@yahoo.com>
2017-05-21 14:43:14 +0300

    1000 de label-uri

--- Client/src/client/RegisterPageController.java
+++ Client/src/client/RegisterPageController.java
@@ -8,6 +8,8 @@ package client;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -16,6 +18,8 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 /**
@@ -30,6 +34,29 @@ public class RegisterPageController implements Initializable {
     private Button backBt;
     @FXML
     private Button signIn;
+    @FXML
+    private Label passLength;
+    @FXML
+    private Label passConfirm;
+    @FXML
+    private Label emptyField;
+    @FXML
+    private Label emailCf;
+    @FXML
+    private Label usernameTaken;
+     @FXML
+    private TextField firstName;
+    @FXML
+    private TextField lastName;
+    @FXML
+    private TextField email;
+    @FXML
+    private TextField username;
+    @FXML
+    private PasswordField password;
+    @FXML
+    private PasswordField cfPassword;
+
 
     @FXML
     void back(ActionEvent event) throws IOException 
@@ -45,21 +72,72 @@ public class RegisterPageController implements Initializable {
         stage.show();
     }
     
+    @FXML
     public void signin(ActionEvent event) throws IOException
     {
-        // verificare date (username + parola)
-        Stage stage = null; 
-        Parent root = null;
+        String mail = email.getText();
         
-        stage=(Stage) signIn.getScene().getWindow();
-        root = FXMLLoader.load(getClass().getResource("gamePage.fxml"));
+        Pattern p = Pattern.compile(".+@.+\\.[a-z]+");
+        Matcher m = p.matcher(mail);
         
-        Scene scene = new Scene(root);
-        stage.setScene(scene);
-        stage.show();
+        boolean matchFound = m.matches();
+        
 
+       if ( !firstName.getText().equals("") && !lastName.getText().equals("") &&  !mail.equals("") && !username.getText().equals("") && !password.getText().equals(""))
+        {    
+            if(matchFound)
+            {
+                if(password.getLength() >= 8)
+                {       
+                    if(password.getText().equals(cfPassword.getText()))
+                    {
+                        
+                        Stage stage = null; 
+                        Parent root = null;
+        
+                        stage=(Stage) signIn.getScene().getWindow();
+                        root = FXMLLoader.load(getClass().getResource("gamePage.fxml"));
+        
+                        Scene scene = new Scene(root);
+                        stage.setScene(scene);
+                        stage.show();
+                        
+                    //daca exista in baza de date
+                    //-> usernameTaken.setVisibility(true);
+                    }
+                    else
+                    {   
+                        emptyField.setVisible(false);
+                        passLength.setVisible(false);
+                        passConfirm.setVisible(true);
+                        emailCf.setVisible(false);
+                    }
+                }
+                else
+                {
+                    emptyField.setVisible(false);
+                    passLength.setVisible(true);
+                    passConfirm.setVisible(false);
+                    emailCf.setVisible(false);
+                }
+            }
+            else
+            {
+                    emptyField.setVisible(false);
+                    passLength.setVisible(false);
+                    passConfirm.setVisible(false);
+                    emailCf.setVisible(true);
+            }  
+        }
+        else
+        {
+            emptyField.setVisible(true);
+            passLength.setVisible(false);
+            passConfirm.setVisible(false);
+            emailCf.setVisible(false);
+        }                      
     }
-    
+   
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
